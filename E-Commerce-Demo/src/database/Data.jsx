0e97871ea48867b0c493204920a0f5278612a592
ReshadDev <reshadqarayev44@gmail.com>
2022-04-23 13:16:20 +0400

    E-Demo

---
+++ E-Commerce-Demo/src/database/Data.jsx
@@ -0,0 +1,156 @@
+import img1 from '../assets/img/ForzaHorizon5.jpg';
+import img2 from '../assets/img/Animals Rivals.jpg';
+import img3 from '../assets/img/CarMechanics2021.jpg';
+import img4 from '../assets/img/F12019.jpg';
+import img11 from '../assets/img/Dirt5.jpg';
+import img12 from '../assets/img/ToyCardOffRoad.jpg';
+import img13 from '../assets/img/WingBreakers.jpg';
+import img14 from '../assets/img/Monster Energy5.jpg';
+import img5 from '../assets/img/Baseball21.jpg'
+import img6 from '../assets/img/WWE2022.jpg'
+import img7 from '../assets/img/RUGBY22.jpg'
+import img8 from '../assets/img/handBall21.jpg';
+import img51 from '../assets/img/Fifa19.jpg'
+import img52 from '../assets/img/Golf21.jpg'
+import img53 from '../assets/img/Pes21.jpg'
+import img54 from '../assets/img/TennisWorlld 2.jpg'
+
+
+
+
+const Data = {
+    CarGames:[
+        {
+            id:1,
+            img: img1,
+            title:"Forza Horizon 5",
+            desc:"Forza Horizon 5 is a 2021 racing video game developed by Playground Games and published by Xbox Game ",
+            type:"PC",
+            price:120
+        },
+        {
+            id:2,
+            img: img2,
+            title:"Animal Rivals",
+            desc:"Animal Rivals is a classic Party Game up to 4 players locally! Grab your friends, pick up some gamepads and ! ",
+            type:"PC",
+            price:30
+        },
+        {
+            id:3,
+            img: img3,
+            title:"Car Mechanics 2021",
+            desc:"Car Mechanic Simulator 2021 is new production with well settled player base. Start as a fresh owner of a car  ",
+            type:"PC",
+            price:40
+        },
+        {
+            id:4,
+            img: img4,
+            title:"F1 2019",
+            desc:"F1 2019 is the official video game of the 2019 Formula One and Formula 2 Championships developed ",
+            type:"PC",
+            price:90
+        },
+        {
+            id:5,
+            img: img11,
+            title:"Dirt 5",
+            desc:"Dirt 5 is a simcade racing video game developed and published by Codemasters. The game was released",
+            type:"PC",
+            price:54
+        },
+        {
+            id:6,
+            img: img12,
+            title:"Super Toycars OffRoad",
+            desc:"Feb 18, 2022. Super Toy Cars Offroad brings the series off the beaten track and into the mud, sand, snow.",
+            type:"PC",
+            price:38
+        },
+        {
+            id:7,
+            img: img13,
+            title:"Wing Breakers",
+            desc:"Wing Breakers is extreme battle air racing mayhem with crazy cows. Survive chaotic air fights full of missile",
+            type:"PC",
+            price:62
+        },
+        {
+            id:8,
+            img: img14,
+            title:"Monster SuperCross",
+            desc:"Monster Energy® AMA Supercross is the most competitive and highest-profile off-road motorcycle racing",
+            type:"PC",
+            price:21
+        }
+    ],
+    SportGames:[
+        {
+            id:1,
+            img: img5,
+            title:"BaseBall 21",
+            desc:"Baseball 21. R.B.I. is the fast-paced, easy-to-learn baseball franchise perfect for anyone who wants to pick up and play. R.B.I. Baseball 21 brings new features",
+            type:"PC",
+            price:110
+        },
+        {
+            id:2,
+            img: img6,
+            title:"WWE 2022",
+            desc:"WWE 2K22 is a professional wrestling video game developed by Visual Concepts and published by 2K Sports. It is the twenty-second overall",
+            type:"PC",
+            price:32
+        },
+        {
+            id:3,
+            img: img7,
+            title:"Rugby 22",
+            desc:"Rugby 22 offers a true rugby experience with a deep tactical system and authenticity that recreates the intensity of the sport's big matches.",
+            type:"PC",
+            price:87
+        },
+        {
+            id:4,
+            img: img8,
+            title:"HandBall 21",
+            desc:"Play thrilling matches against the top European teams and experience handball like you are actually there! On the court, implement your game plan ",
+            type:"PC",
+            price:99
+        },
+        {
+            id:5,
+            img: img51,
+            title:"Fifa 2019",
+            desc:"FIFA 19 is a football simulation video game developed by EA Vancouver and released by Electronic Arts on 28 September 2018",
+            type:"PC",
+            price:150
+        },
+        {
+            id:6,
+            img: img52,
+            title:"Golf 2021",
+            desc:"PGA Tour 2K21 is a sports video game developed by HB Studios and published by 2K Sports for Stadia, Microsoft Windows, Nintendo Switch. ",
+            type:"PC",
+            price:74
+        },
+        {
+            id:7,
+            img: img53,
+            title:"eFootball PES 2021",
+            desc:"eFootball PES 2021 is a football simulation video game developed by PES Productions and published by Konami for Microsoft Windows",
+            type:"PC",
+            price:134
+        },
+        {
+            id:8,
+            img: img54,
+            title:"Tennis WorldTour 2",
+            desc:"Tennis world tour 2 will give you the opportunity to experience the purest form of tennis, with faster paced gameplay and enhanced animations  ",
+            type:"PC",
+            price:91
+        }
+    ]
+}
+
+export default Data
